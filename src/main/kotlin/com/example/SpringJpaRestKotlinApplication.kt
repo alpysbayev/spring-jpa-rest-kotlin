3d8c0ffe141553b9abee5e373745503a6aac6b09
alpysbayev <adilet.alpysbayev@gmail.com>
2022-11-23 22:24:52 +0600

    first commit

---
+++ src/main/kotlin/com/example/SpringJpaRestKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJpaRestKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringJpaRestKotlinApplication>(*args)
+}
